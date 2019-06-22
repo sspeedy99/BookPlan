52fcea83369310e86169628134fe9489a9be19e1
sspeedy <sspeedysingh096@gmail.com>
2019-06-23 00:58:14 +0530

    added condition for action type

--- src/store/reducers/bookReducer.js
+++ src/store/reducers/bookReducer.js
@@ -8,6 +8,13 @@ const initState = {
 
 
 const bookReducer = (state = initState, action) => {
+    // eslint-disable-next-line default-case
+    switch(action.type)
+    {
+        //checking if the action type of the dispatch is create a new book, if yes then log it to the console
+        case 'CREATE_BOOK':
+        console.log('created a new book', action.book);
+    }   
     return state
 }
 
