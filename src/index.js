4880400ccf79ad68f92b3a484ab67d3992e90e9a
sspeedy <sspeedysingh096@gmail.com>
2019-06-23 01:48:35 +0530

    added store enhancer to the redux store

--- src/index.js
+++ src/index.js
@@ -3,14 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux'; 
+import { createStore, applyMiddleware, compose } from 'redux'; 
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { reduxFirestore,getFirestore } from 'redux-firestore';
+import { reactReduxFirebase,getFirebase } from 'react-redux-firebase';
+import firebaseConfig from './config/firebaseConfig';
 
 
 //creating a redux store and passing thunk as our middleware
-const store =  createStore(rootReducer, applyMiddleware(thunk));
+//compose will join two or more store enhancers and in one store
+const store =  createStore(rootReducer, 
+    compose(
+        applyMiddleware(thunk.withExtraArgument({getFirebase,getFirestore})),
+        reduxFirestore(firebaseConfig),
+        reactReduxFirebase(firebaseConfig)
+    )
+    );
 
 
 ReactDOM.render(<Provider store = {store}><App /></Provider>, document.getElementById('root'));
--- src/store/actions/bookAction.js
+++ src/store/actions/bookAction.js
@@ -2,7 +2,7 @@
 
 export const createBook = (book) => {
     // dispatch property sends state to reducer
-    return (dispatch, getState) => {
+    return (dispatch, getState,{getFirebase,getFirestore}) => {
         //pausing the dispatch and sending 
       // make async call to database
       dispatch({ type: 'CREATE_BOOK', book });
