67cc20bf3033c08889fbf8307416e5f4377fe636
sspeedy <sspeedysingh096@gmail.com>
2019-07-01 23:44:12 +0530

    Added dynamic content

--- src/components/books/BookDetails.js
+++ src/components/books/BookDetails.js
@@ -1,21 +1,50 @@
 import React from 'react'
+import {connect} from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 
 const BookDetails = (props) => {
-    const id = props.match.params.id;
-    return (
+    const { book } = props;
+    if(book) {
+        return(
         <div className="container section project-details">
            <div className="card z-depth-0">
                <div className="card-content">
-                    <span className="card-title">Book Title - {id}</span>
-                    <p>Quis excepteur sunt nisi cupidatat ipsum deserunt ad quis. Ad fugiat enim culpa culpa id pariatur ex ut. Eu cupidatat magna incididunt dolore magna do eu deserunt ex.</p>
+                    <span className="card-title">{book.title}</span>
+                    <p>{book.content}</p>
                </div>
                <div className="card-action grey lighten-4 grey-text">
-                   <div>Posted by Shashi Prakash</div>
+                   <div>Posted by {book.authorFirstName} {book.authorLastName}</div>
                    <div>2nd, September, 2am</div>
                </div>
             </div> 
         </div>
-    )
+        )
+        
+    } 
+    else {
+        return (
+            <div className="container center">
+                <p>Loading Book Details...</p>
+            </div>
+            )
+    }
+   
 }
 
-export default BookDetails;
+const mapStateToProps = (state, ownProps) => {
+    const id = ownProps.match.params.id;
+    const books = state.firestore.data.books;
+    const book = books ? books[id] : null
+    return {
+        // reading from firestore property of the root reducer
+        book : book
+    }
+}
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([ 
+        { collection:'books' }
+    ])
+)(BookDetails);
--- src/components/books/BookList.js
+++ src/components/books/BookList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import BookSummary from './BookSummery'
+import { Link } from 'react-router-dom';
 
 const BookList = ({books}) => {
     return (
@@ -7,7 +8,7 @@ const BookList = ({books}) => {
             {books && books.map(book => {
                 return (
                     //passing each book into BookSummary.
-                    <BookSummary book={book} key = {book.id}/>
+                    <Link to ={'/books/'+ book.id}  > <BookSummary book={book} key = {book.id}/> </Link>
                 )
             })}
         </div>
