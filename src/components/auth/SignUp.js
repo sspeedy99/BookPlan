95df48cf5b5a924226c3456f650c0ac3d1027157
sspeedy <sspeedysingh096@gmail.com>
2019-07-04 00:18:13 +0530

    added route gaurd

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import {connect} from 'redux';
+import {connect} from 'react-redux';
+import {Redirect} from 'react-router-dom'
 
 class SignUp extends Component {
     state = {
