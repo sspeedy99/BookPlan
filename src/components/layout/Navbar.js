3aefb217811b38c6a59436d782bc1d829f0ee9e7
sspeedy <sspeedysingh096@gmail.com>
2019-07-03 22:25:37 +0530

    added conditional navbar links

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -6,14 +6,18 @@ import SignedOutLinks from './SignOutLink';
 import { connect } from 'react-redux';
 
 const Navbar = (props) => {
+    const {auth} = props
+    //Checkin if the user is signed in or not
+    //if the user is signed in then show SignedLinks or SignedOut
+    //If the user is signed in then the uid will not be null
+    const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
   return (
     //Materialize CSS Wrapper with nav
     <nav className="nav-wrapper grey darken-4">
       <div className="container">
         {/*Hyperlink to the brand title of the page*/ }
         <Link className="brand-logo" to="/">BookPlan</Link>
-        <SignedInLinks/>
-        <SignedOutLinks/>
+        {links}
       </div>
     </nav>
   )
